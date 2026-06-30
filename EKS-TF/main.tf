23b58e426d76635d5a9c6da28cc24b00ffbe95ec
Muhammad Faiz <77822431+flamefaiz@users.noreply.github.com>
2026-06-30 22:36:44 +0800

    Update security group reference in main.tf

--- EKS-TF/main.tf
+++ EKS-TF/main.tf
@@ -164,7 +164,7 @@ resource "aws_eks_cluster" "eks-cluster" {
   role_arn = aws_iam_role.EKSClusterRole.arn
   vpc_config {
     subnet_ids         = [aws_subnet.subnet.id, aws_subnet.public-subnet2.id]
-    security_group_ids = [aws_security_group.sg-default.id]
+    security_group_ids = [aws_security_group.security-group.id]
   }
 
   version = 1.33
