d674d6b0b839132ae125b15e78c901605d5f53a6
Muhammad Faiz <77822431+flamefaiz@users.noreply.github.com>
2026-06-30 22:28:35 +0800

    Update main.tf

--- EKS-TF/main.tf
+++ EKS-TF/main.tf
@@ -102,6 +102,37 @@ resource "aws_route_table" "rt2" {
     Name = var.rt-name2
   }
 }
+resource "aws_iam_role" "EKSClusterRole" {
+  name = "EKSClusterRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role" "NodeGroupRole" {
+  name = "EKSNodeGroupRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 
 resource "aws_route_table_association" "rt-association2" {
   route_table_id = aws_route_table.rt2.id
@@ -127,3 +158,4 @@ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.NodeGroupRole.name
 }
+
