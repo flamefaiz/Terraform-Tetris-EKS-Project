baa85b2d4b4a9a3fe8efe6a33bd32c56b4f30599
Muhammad Faiz <77822431+flamefaiz@users.noreply.github.com>
2026-06-30 22:35:45 +0800

    Update variables.tf

--- EKS-TF/variables.tf
+++ EKS-TF/variables.tf
@@ -5,7 +5,6 @@ variable "rt-name2" {}
 variable "subnet-name" {}
 variable "subnet-name2" {}
 variable "sg-name" {}
-variable "security-group-name" {}
 variable "iam-role-eks" {}
 variable "iam-role-node" {}
 variable "iam-policy-eks" {}
